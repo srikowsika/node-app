28b50e47285c12827741043e264e142108e55f97
srikowsika <srikowsika.guna@aspiresys.com>
2019-10-22 20:10:59 +0530

    Heroku - NodeJS

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
-const { Client } = require('pg'); 
-const axios = require('axios');
-const config=require('./config/config')
 
-axios.get('http://www.mocky.io/v2/5dad8c282d0000a542e4babb')
+const { Client } = require('pg');
+const axios = require('axios');
+const config = require('./config/config')
+const query = require('./query/query')
+
+
+axios.get(config.url.api)
   .then(response => {
     res = response.data
+    console.log(res.id)
     const client = new Client({
-      connectionString:config.url.connectionString,
+      connectionString: config.url.connectionString,
       ssl: true,
     });
-    //console.log(client.connectionString)
+
     client.connect();
-    // client.query('Create table Student(id integer,name varchar(50));', (err, res) => {
-    //   if (err) throw err;
-    //   console.log(res)
-      
-    // });
-    // client.query('INSERT INTO Student VALUES ($2,$1)', [response.data.name, response.data.EmpId], (err, res) => {
-    //   if (err) throw err;
-    //   console.log(res)
-    //  });
-    client.query('Select * from Employee;', (err, res) => {
+    client.query(query.get_tables, ['public'], (err, res) => {
       if (err) throw err;
-      //sconsole.log(res)
+      for (let row of res.rows) {
+        if (row.table_name !== 'student') {
+          client.query(query.query.create_student_table, (err, res) => {
+            if (err) throw err;
+          });
+        }
+      }
+    })
+    client.query(query.insert_values, [res.id, res.name], (err, res) => {
+      if (err) throw err;
+    });
+
+    client.query(query.select_table, (err, res) => {
+      if (err) throw err;
       for (let row of res.rows) {
         console.log(row)
       }
