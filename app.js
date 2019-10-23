8f31406585d61eb4cadcd66254be7e60f041026a
srikowsika <srikowsika.guna@aspiresys.com>
2019-10-23 13:11:15 +0530

    updated

--- app.js
+++ app.js
@@ -1,32 +1,43 @@
-
+const express=require('express');
+const app=express();
 const { Client } = require('pg');
 const axios = require('axios');
 const config = require('./config/config')
 const query = require('./query/query')
 
+app.use(express.static('index.html'))
 
 axios.get(config.url.api)
   .then(response => {
-    res = response.data
-    console.log(res.id)
+    http_res = response.data
+    console.log(http_res)
     const client = new Client({
       connectionString: config.url.connectionString,
       ssl: true,
     });
-
+    
     client.connect();
     client.query(query.get_tables, ['public'], (err, res) => {
       if (err) throw err;
+      console.log(res);
       for (let row of res.rows) {
         if (row.table_name !== 'student') {
-          client.query(query.query.create_student_table, (err, res) => {
+          client.query(query.create_student_table, (err, res) => {
             if (err) throw err;
+            console.log(res);
           });
         }
       }
     })
-    client.query(query.insert_values, [res.id, res.name], (err, res) => {
+    
+    // client.query('Drop table Student_Detail', (err, res) => {
+    //   if (err) throw err;
+    //   //console.log(res)
+      
+    // });
+    client.query(query.insert_values, [http_res.Id, http_res.Name,http_res.Type,http_res.attributes], (err, res) => {
       if (err) throw err;
+      console.log(res)
     });
 
     client.query(query.select_table, (err, res) => {
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-// const http = require('http');
-// const app = require('./app');
-// const port = process.env.PORT || 4000;
-// const server = http.createServer(app);
-// server.listen(port);
+const http = require('http');
+const app = require('./app');
+const port = 4000;
+const server = http.createServer(app);
+server.listen(port);
 
-// console.log('Server Loaded');
+console.log('Server Loaded');
